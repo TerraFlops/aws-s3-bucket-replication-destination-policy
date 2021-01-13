234345a7f7bd3c13827d56d2cb4032b7d7f54aec
Damian Sloane <damian@capacitorlabs.com>
2021-01-13 11:29:12 +1100

    Work in progress

--- output.tf
+++ output.tf
@@ -2,8 +2,3 @@ output "json" {
   description = "S3 bucket policy that should be applied to destniation bucket to allow replication"
   value = data.aws_iam_policy_document.destination.json
 }
-
-output "override_json" {
-  description = "S3 bucket policy that should be applied to destniation bucket to allow replication"
-  value = data.aws_iam_policy_document.destination.override_json
-}
